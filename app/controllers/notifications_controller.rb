5929a31df059b0cbca8cbd2cd6dac479136769c8
Bhargav-Thummar <bhargav.thummar810@gmail.com>
2023-01-23 11:23:48 +0530

    feature: add/remove alert-bedge for notification icon on creation of new bid using turbo

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,10 +2,15 @@ class NotificationsController < ApplicationController
   before_action :set_object, only: [:destroy]
 
   def destroy
-    if @notification.destroy!
-      render json: { notification_id: @notification.id, status: 200, message: "Notification successfully removed!" }
-    else
-      render json: { notification_id: @notification.id, status: 403, message: "Notification failed to remove!" }
+    respond_to do |format|
+      if @notification.destroy!
+        format.json { render json: { notification_id: @notification.id, status: 200, message: "Notification successfully removed!" } }
+        # format.turbo_stream { render turbo_stream: turbo_stream.remove(@notification) }
+      else
+        format.json { render json: { notification_id: @notification.id, status: 403, message: "Notification failed to remove!" } }
+        # flash.now[:alert] = "Notification failed to remove!"
+        # format.turbo_stream { render turbo_stream: turbo_stream.prepend("flash", partial: "layouts/flash") }
+      end
     end
   end
 
